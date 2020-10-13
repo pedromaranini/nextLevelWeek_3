dc9fefdf27c8c2304374889cc4d55d36051a8b20
pedromaranini <pedrolucasmaranini30@gmail.com>
2020-10-13 19:22:03 -0300

    creating landingpage and map

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../assets/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
+
+function OrphanagesMap() {
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="" />
+
+                    <h2>Escolha uma orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Mongaguá</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+
+            <Map  
+                center={[-24.0891933,-46.7437005]}
+                zoom={12}
+                style={{ width: '100%', height: '100%' }}
+            > 
+                /** Versão mapa padrão */
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+                /** Versão mapa darkmode */
+                {/* <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} /> */}
+
+                /** Versão mapa satelite */
+                {/* <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} /> */}
+            </Map>
+
+            <Link to="" className="create-orphanage" >
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
+    );
+}
+
+export default OrphanagesMap;
